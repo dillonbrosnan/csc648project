15984c77fba459cac04d8723890f93c3cc2752bc
vikramgrewal <vik.grewal1992@gmail.com>
2017-10-23 20:45:54 -0700

    Test with fake data

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.get('/', function(req, res) {
   db.query(sql, function (err, rows) {
       if (err) throw err;
       if(rows.length != 0){
-          res.render('index');
+          res.render('index', { name: 'Tobi' });
         }else{
-          res.render('index');
+          res.render('index', { name: 'Tobi' });
       }
   })
 });
