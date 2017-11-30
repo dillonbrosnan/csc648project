b9955e97bc8a9fe89dcade5198a917536b974b5a
vikramgrewal <vik.grewal1992@gmail.com>
2017-11-29 19:18:14 -0800

    Fixed db.js hopefully

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var db_config = {
 	host: "127.0.0.1",
-	user: "root",
+	user: "fa17g07",
 	password: "csc648fa17g07",
 	database: 'fa17g07',
 	connectionLimit : 10,               // this is the max number of connections before your pool starts waiting for a release
