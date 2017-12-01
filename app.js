ca06f090e24f3b1f9feb01c7d5703817a5e3e93d
vikramgrewal <vik.grewal1992@gmail.com>
2017-11-30 18:34:32 -0800

    Modified static files

--- app.js
+++ app.js
@@ -14,7 +14,74 @@ var port = process.env.PORT || 17007;
 //App delcaration
 var app = express();
 
+var express = require('express');
+var db = require('./db');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var validator = require('express-validator');
+var session = require('express-session');
+var busboyBodyParser = require('busboy-body-parser');
+var fileUpload = require('express-fileupload');
+var port = process.env.PORT || 17007;
+// var router = express.Router();
+
+
+//App delcaration
+var app = express();
+
 app.use(express.static('/fa17g07/public'));
+
+app.set('view engine', 'ejs');
+app.set('port', port);
+app.use(fileUpload());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(busboyBodyParser());
+app.use(validator());
+app.use(session({
+	cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    secret: '4F33-5ZDE-ZggpE3D',
+    resave: true,
+    saveUninitialized: true
+}));
+var server = require('http').createServer(app);  
+
+//All routes for application
+var index = require('./routes/index.js');
+var sale = require('./routes/sale.js');
+var portfolio = require('./routes/portfolio.js');
+var login = require('./routes/login.js');
+var register = require('./routes/register.js');
+var team = require('./routes/team.js');
+var post = require('./routes/post.js');
+var userChat = require('./routes/userChat.js');
+var saleListing = require('./routes/saleListing.js');
+
+<!-- Routes -->
+app.use('', index);
+app.use('/forSale', sale);
+app.use('', portfolio);
+app.use('/login', login);
+app.use('/register', register);
+app.use('/team', team);
+app.use('/agent/post', post);
+app.use('/user/message', userChat);
+app.use('/forSale', saleListing);
+
+
+<!-- Listening port -->
+server.listen(port, function () {
+  console.log('Real estate app listening on port 17007!');
+})
+
+
+
+
+app.use('/public', express.static('fa17g07/public'));
+app.use('/css', express.static('fa17g07/public/css'));
+
+// app.use(express.static('/fa17g07/public'));
 
 app.set('view engine', 'ejs');
 app.set('port', port);
