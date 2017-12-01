152bf1320c0c16e254d48328a5f3f9361cefa641
vikramgrewal <vik.grewal1992@gmail.com>
2017-11-30 19:16:55 -0800

    Modified static files

--- app.js
+++ app.js
@@ -55,8 +55,7 @@ var post = require('./routes/post.js');
 var userChat = require('./routes/userChat.js');
 var saleListing = require('./routes/saleListing.js');
 
-app.use('/public', express.static('fa17g07/public'));
-app.use('/css', express.static('fa17g07/public/css'));
+app.use('/public', express.static('public'));
 
 <!-- Routes -->
 app.use('', index);
