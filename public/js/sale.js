73064eb3bac9b669b78f2eb6916901652c31ad24
royanguiano <roya@tuborrow.com>
2017-12-05 12:19:16 -0800

    fixing path issues

--- public/js/sale.js
+++ public/js/sale.js
@@ -18,10 +18,10 @@ if($( window ).width() >= 992)  {
 });
 
 
-    //set distance to every location on window load
-    (function addEventsAndStuff() {
-     setLocationforListing(); 
-    })();
+    // //set distance to every location on window load
+    // (function addEventsAndStuff() {
+    //  setLocationforListing(); 
+    // })();
 
     //sort listings based on price acending order
     function acendingsort() {
@@ -84,60 +84,60 @@ if($( window ).width() >= 992)  {
          document.getElementById("sortingbutton").setAttribute('onclick',  'acendingsort()');
          document.getElementById("sortingbutton").innerHTML = 'Prices <i class="fa fa-angle-double-down" aria-hidden="true">';
     }
-    //convert lat and lng to distance from each search result
-    function getDistance(lat2,lon2) {
-      let lat1 = 37.77397, 
-          lon1 = -122.431297;
+    // //convert lat and lng to distance from each search result
+    // function getDistance(lat2,lon2) {
+    //   let lat1 = <%- lat %>, 
+    //       lon1 = <%- lon %>;
           
-      var R = 6371; // Radius of the earth in km
-      var dLat = deg2rad(lat2-lat1);  // deg2rad below
-      var dLon = deg2rad(lon2-lon1); 
-      var a = 
-              Math.sin(dLat/2) * Math.sin(dLat/2) +
-              Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-              Math.sin(dLon/2) * Math.sin(dLon/2); 
+    //   var R = 6371; // Radius of the earth in km
+    //   var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    //   var dLon = deg2rad(lon2-lon1); 
+    //   var a = 
+    //           Math.sin(dLat/2) * Math.sin(dLat/2) +
+    //           Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    //           Math.sin(dLon/2) * Math.sin(dLon/2); 
 
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-      var d = R * c; // Distance in km
-      return d;
-    }
-    //convert degrees to radians
-    function deg2rad(deg) {
-      return deg * (Math.PI/180)
-    }
+    //   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    //   var d = R * c; // Distance in km
+    //   return d;
+    // }
+    // //convert degrees to radians
+    // function deg2rad(deg) {
+    //   return deg * (Math.PI/180)
+    // }
 
-    //sets the distance property to each listing 
-    function setLocationforListing(){
-      var listSection = document.getElementsByClassName('listContainer');
+    // //sets the distance property to each listing 
+    // function setLocationforListing(){
+    //   var listSection = document.getElementsByClassName('listContainer');
         
-        for(let i = 0; i < listSection[0].children.length; i++){
-          if(listSection[0].children[i].className == 'row'){
-            var parent = listSection[0].children[i]; 
-          }
-        }
+    //     for(let i = 0; i < listSection[0].children.length; i++){
+    //       if(listSection[0].children[i].className == 'row'){
+    //         var parent = listSection[0].children[i]; 
+    //       }
+    //     }
 
-        var children = [];
-        //get all childNodes
-        for (var i = 0; i < parent.childNodes.length; i++) {
-            if (parent.childNodes[i].className == "col-lg-4 col-md-4 col-sm-6") {
-              children.push(parent.childNodes[i]); 
-            }        
-        }
+    //     var children = [];
+    //     //get all childNodes
+    //     for (var i = 0; i < parent.childNodes.length; i++) {
+    //         if (parent.childNodes[i].className == "col-lg-4 col-md-4 col-sm-6") {
+    //           children.push(parent.childNodes[i]); 
+    //         }        
+    //     }
 
 
-        let childrenLength = children.length; 
-        var location = [], i, len;
+    //     let childrenLength = children.length; 
+    //     var location = [], i, len;
         
-        for (i = 0, len = children.length; i < len; i++) {
-          location.push(getDistance(children[i].attributes[2].nodeValue, children[i].attributes[3].nodeValue));
-        }
+    //     for (i = 0, len = children.length; i < len; i++) {
+    //       location.push(getDistance(children[i].attributes[2].nodeValue, children[i].attributes[3].nodeValue));
+    //     }
 
-        for(let i = 0; i < location.length; i++){
-          var att = document.createAttribute("distance");       // Create a "class" attribute
-          att.value = location[i];                           // Set the value of the class attribute
-          children[i].setAttributeNode(att);                          // Add the class attribute to <h1>
-        }
-    }
+    //     for(let i = 0; i < location.length; i++){
+    //       var att = document.createAttribute("distance");       // Create a "class" attribute
+    //       att.value = location[i];                           // Set the value of the class attribute
+    //       children[i].setAttributeNode(att);                          // Add the class attribute to <h1>
+    //     }
+    // }
 
     //find houses nearest the users search input location
     function locationDecendingSort() {
