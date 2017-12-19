26d1bf95f3dbba36f19645b66c68325665b1bd5a
vikramgrewal <vik.grewal1992@gmail.com>
2017-12-18 17:12:34 -0800

    Successfully edit profile

--- routes/editProfile.js
+++ routes/editProfile.js
@@ -4,22 +4,39 @@ var pool = require('../db');
 var EditProfileModel = require('../models/editProfileModel.js');
 
 // Route
-router.get('agent/editProfile',function(req,res){
-  if(req.session.isLoggedIn && req.session.role != 'agent')  {
+router.get('/agent',function(req,res){
+  if(!req.session.isLoggedIn || req.session.role != 'agent')  {
     return res.redirect('../..');
-  }
-  res.render('agentEditProfile', {role: "agent"} );
+  } else if(req.session.isLoggedIn && req.session.role == 'agent')
+    var agentId = req.session.sessionId;
+    EditProfileModel.getAgentInfo(agentId)
+    .then(function(agentInfo)  {
+      res.render('editProfile', {
+        role: req.session.role,
+        id: req.session.sessionId,
+        info: agentInfo[0]
+      });
+    })
+  
 });
 
-router.get('editProfile',function(req,res){
-  if(req.session.isLoggedIn && req.session.role != 'user')  {
-    return res.redirect('..');
-  }
-  res.render('userEditProfile', {role: "user"});
+router.get('/',function(req,res){
+  if(!req.session.isLoggedIn || req.session.role != 'user')  {
+    return res.redirect('../..');
+  } else if(req.session.isLoggedIn && req.session.role == 'user')
+    var userId = req.session.sessionId;
+    EditProfileModel.getUserInfo(userId)
+    .then(function(userInfo)  {
+      res.render('editProfile', {
+        role: req.session.role,
+        id: req.session.sessionId,
+        info: userInfo[0]
+      });
+    })
 });
 
 // Route
-router.post('editProfile',function(req,res){
+router.post('/',function(req,res){
 
   var username = req.body.username;
   var firstName = req.body.firstName;
@@ -30,7 +47,7 @@ router.post('editProfile',function(req,res){
   var lng = Number(req.body.lng);
   var userId = req.session.sessionId;
 
-  RegisterModel.userCheck(username, email)
+  EditProfileModel.userCheck(username, email, userId)
     .then(function(duplicateUsers)  {
         if(duplicateUsers.length > 0) { // Duplicate users check
           if(duplicateUsers[0].username == username && duplicateUsers[0].email == email)  {
@@ -45,19 +62,16 @@ router.post('editProfile',function(req,res){
               email: email
             })
           } else  {
-            return res.render('register', {
-              role: 'user',
-              error: 'Email ' + email + ' taken',
-              username: username
-            })
+            return res.redirect('../editProfile');
           }
         } else  {
-          return RegisterModel.insertUser(userId, hash, email, firstName, 
-            lastName, username, lat, lng, formattedAddress);
+          return EditProfileModel.updateValuesUser(email, firstName, 
+            lastName, username, lat, lng, formattedAddress, userId);
         }
     })
     .then(function(insertedRows)  {
-        return res.redirect('../login');
+      console.log(insertedRows);
+        return res.redirect('../editProfile');
     })
     .catch(function(error)  {
       return res.status(500).send( { error : error });
@@ -66,57 +80,50 @@ router.post('editProfile',function(req,res){
 });
 
 // Route
-router.post('agent/editProfile',function(req,res){
+router.post('/agent',function(req,res){
 
   var username = req.body.username;
-  var password = req.body.password;
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var email = req.body.email;
   var formattedAddress = req.body.formattedAddress;
   var lat = Number(req.body.lat);
   var lng = Number(req.body.lng);
-  var userId = uuidv4({msecs: new Date().getTime()});
+  var agentId = req.session.sessionId;
 
-  var salt = bcrypt.genSaltSync(10);
-  // Salt and hash password
-  var hash = bcrypt.hashSync(password, salt);
-
-  RegisterModel.agentCheck(username, email)
+  EditProfileModel.agentCheck(username, email, agentId)
     .then(function(duplicateUsers)  {
         if(duplicateUsers.length > 0) { // Duplicate users check
           if(duplicateUsers[0].username == username && duplicateUsers[0].email == email)  {
             return res.render('register', {
-              role: 'agent',
+              role: 'user',
               error: 'Username ' + username + ' and email ' + email + ' taken'
             })
           } else if(duplicateUsers[0].username == username) {
             return res.render('register', {
-              role: 'agent',
+              role: 'user',
               error: 'Username ' + username + 'taken',
               email: email
             })
           } else  {
-            return res.render('register', {
-              role: 'agent',
-              error: 'Email ' + email + ' taken',
-              username: username
-            })
+            return res.redirect('../editProfile/agent');
           }
         } else  {
-          return RegisterModel.insertAgent(userId, hash, email, firstName, 
-            lastName, username, lat, lng, formattedAddress);
+          return EditProfileModel.updateValuesAgent(email, firstName, 
+            lastName, username, lat, lng, formattedAddress, agentId);
         }
     })
     .then(function(insertedRows)  {
-        return res.redirect('../../login/agent');
+      console.log(insertedRows);
+        return res.redirect('../editProfile/agent');
     })
     .catch(function(error)  {
       return res.status(500).send( { error : error });
     })
 
-
 });
+
+
 
 
 module.exports = router;
