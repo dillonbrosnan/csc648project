05eaab19da3fb8ac4804ec62939b2a15ecb86b19
dillonbrosnan <dillonbrosnan@yahoo.com>
2017-12-18 17:04:30 -0800

    fixing logout redirect

--- routes/logout.js
+++ routes/logout.js
@@ -5,10 +5,10 @@ router.get('/', function(req,res){
 
 	if(req.session.isLoggedIn)	{
 		req.session.destroy();
-		return res.redirect('..');
+		return res.redirect('../../../fa17g07/');
 	}
 	
-	return res.redirect('..');
+	return res.redirect('../../../fa17g07/');
 });
 
 module.exports = router;
